99d5aa2fa3c7aae0f8bde302f9997dcd5ef64507
giovaniMurakami <giovani.murakami@outlook.com>
2023-07-17 11:42:35 -0300

    Revert "Adicionado arquivo aula-01"
    This reverts commit 4f8546d84a62ea88b02d6e0332910d7c7e5e1615.

--- section2-Basics/aula01/consoleLog.js
+++
@@ -1,6 +0,0 @@
-console.log("Hello, World!") //Strings
-console.log('Hello, World!')
-console.log(`Hello, World!`) //Suporta, dentro do seu texto, aspas simples e/ou aspas duplas
-
-
-console.log(12345)
