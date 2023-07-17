4f8546d84a62ea88b02d6e0332910d7c7e5e1615
giovaniMurakami <giovani.murakami@outlook.com>
2023-07-17 11:33:52 -0300

    Adicionado arquivo aula-01

---
+++ section2-Basics/aula01/consoleLog.js
@@ -0,0 +1,6 @@
+console.log("Hello, World!") //Strings
+console.log('Hello, World!')
+console.log(`Hello, World!`) //Suporta, dentro do seu texto, aspas simples e/ou aspas duplas
+
+
+console.log(12345)
